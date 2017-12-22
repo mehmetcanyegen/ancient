507c47b5641dff03b34b0feafe4b72394974dac3
mehmetcanyegen <mehmetcan.yegen@gmail.com>
2017-12-22 05:33:34 +0300

    first commit, added MsSql

---
+++ ancient/Db/MsSql.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ancient.Db
+{
+    class MsSql
+    {
+
+        public static string Con = "";
+
+        public void Ex(string sql, SqlParameter[] spr = null)
+        {
+            using (var con = new SqlConnection(Con))
+            {
+                con.Open();
+                var cmd = new SqlCommand(sql, con);
+                if (spr != null)
+                    cmd.Parameters.AddRange(spr);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        public SqlDataReader ExDr(string sql, SqlParameter[] spr = null)
+        {
+            var con = new SqlConnection(Con);
+            con.Open();
+            var cmd = new SqlCommand(sql, con);
+            if (spr != null)
+                cmd.Parameters.AddRange(spr);
+            var rd = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            return rd;
+        }
+    }
+}
